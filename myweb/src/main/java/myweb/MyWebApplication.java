195fd75afb02baf51db9385ee52d2c0522b8ecaa
Bella <shdudtnr3939@naver.com>
2022-02-10 16:26:15 +0900

    22.02.10 spring boot 연습

---
+++ myweb/src/main/java/myweb/MyWebApplication.java
@@ -0,0 +1,39 @@
+package myweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+public class MyWebApplication {
+
+	@Controller 
+	public class HelloController{
+		
+		@GetMapping("/hello")
+		public String hello() {
+			return "hello";
+		}
+	}
+	
+	@RestController
+	public class HelloController2{
+		
+//		@ResponseBody
+		@GetMapping("/hello2")
+		public String hello2() {
+			return "<h1>Hello World</h1>";
+		}
+	}
+	
+	public static void main(String[] args) {
+//		try (ConfigurableApplicationContext c = SpringApplication.run(MyWebApplication.class, args);) {
+//
+//		}
+		SpringApplication.run(MyWebApplication.class, args);
+	}
+	
+}
